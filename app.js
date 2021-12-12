fe30be9a6b6a1194c534b30fd41791c550ac1cf4
avish mehta <avish@logicwind.co>
2021-12-12 17:08:04 +0530

    changes

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const app = express()
 /**
  * Backend flow:
  * - check to see if the game ID encoded in the URL belongs to a valid game session in progress. 
- * - if yes, join the client to that game. 
- * - else, create a new game instance. 
- * - '/' path should lead to a new game instance. 
+ * - if yes, join the client to that game else create a new game session and join the client to that game.
  * - '/game/:gameid' path should first search for a game instance, then join it. Otherwise, throw 404 error.  
  */
 
